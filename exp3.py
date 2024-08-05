35a03c5de784f782d6d67b53025adaa6f1040093
sanika185 <113962395+sanika185@users.noreply.github.com>
2024-08-04 23:11:34 -0700

    data csv file

---
+++ exp3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Data preprocessing tools
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#Importing the dataset
+dataset = pd.read_csv('data.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+print(X)
+print(Y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
